0e97d801d86d00f7286c5407af56237031aac07a
Thibaut Tiberghien <thibaut@planecq.com>
2017-06-15 21:48:32 +0800

    Route masking

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const { string } = PropTypes
 
 const PostLink = props => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
       <a>{props.title}</a>
     </Link>
   </li>
@@ -22,9 +22,9 @@ const Index = () => (
   <div>
     <h1>My Blog</h1>
     <ul>
-      <PostLink title='Hello Next.js' />
-      <PostLink title='Learn Next.js is awesome' />
-      <PostLink title='Deploy apps with Zeit' />
+      <PostLink id='hello-nextjs' title='Hello Next.js' />
+      <PostLink id='learn-nextjs' title='Learn Next.js is awesome' />
+      <PostLink id='deploy-nextjs' title='Deploy apps with Zeit' />
     </ul>
   </div>
 )
